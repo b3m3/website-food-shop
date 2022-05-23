2aad1098e86b14913b5e31314e4c92c6658457c0
b3m3 <b3m3@yandex.ru>
2022-05-23 21:50:31 +0300

    create create-products.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,13 @@
 import webp from './lib/webp.js';
-import slider from './lib/slider.js';
 import burger from './modules/burger.js';
 import tabs from './modules/tabs.js';
 import select from './modules/select.js';
+import createPoducts from './modules/create-products.js';
 
-webp();
-burger();
-slider();
-tabs();
-select();
+document.addEventListener('DOMContentLoaded', () => {
+  webp();
+  burger();
+  tabs();
+  select();
+  createPoducts();
+});
---
+++ src/js/modules/create-products.js
@@ -0,0 +1,79 @@
+import slider from '../lib/slider.js';
+
+const createPoducts = () => {
+  
+  const getData = async () => {
+    const res  = await fetch('./files/products.json');
+    return await res.json();
+  };
+
+  const block = (wrapp, title) => {
+    const item = document.createElement('div');
+    item.classList.add('products__item', 'cold-snacks');
+    item.innerHTML = `
+      <div class="products__header header-products container">
+        <h2 class="header-products__title title">${title}</h2>
+      </div>
+      <div class="products__cards swiper">
+        <div class="products__wrapp swiper-wrapper">
+        </div>
+      </div>
+    `;
+    wrapp.append(item);
+  };
+
+  const productCard = (wrapp, {id, img, subtitle, weight, desc, price}) => {
+    const card = document.createElement('div');
+    card.classList.add('card', 'swiper-slide');
+    card.id = id;
+    card.innerHTML = `
+      <span class="card__counter">0</span>
+      <div class="card__image ibga">
+        <img src="img/products/${img}" alt="image">
+      </div>
+      <div class="card__body">
+        <div class="card__body-top">
+          <h5 class="card__body-title">${subtitle}</h5>
+          <p class="card__body-wages">Вес: <span>${weight}</span>г</p>
+        </div>
+        <p class="card__body-text">${desc}</p>
+        <div class="card__body-bottom">
+          <p class="card__body-price"><span>${price}</span> $</p>
+          <button class="card__body-btn btn">В корзину <span class="icon-buy"></span></button>
+        </div>
+        <div class="card__body-hover">
+          <button class="card__body-minus btn">-</button>
+          <p class="card__body-price"><span>${price}</span> ₽</p>
+          <button class="card__body-plus btn">+</button>
+        </div>
+      </div>
+    `;
+    wrapp.append(card);
+  };
+
+  getData()
+    .then((data) => {
+      const productsData = [];
+
+      for (const blocks in data) {
+        const wrappBlocks = document.querySelector('.products__main');
+        block(wrappBlocks, data[blocks][0].title);
+        productsData.push(data[blocks][1]);
+      }
+
+      const wrappsProsucts = document.querySelectorAll('.products__wrapp');
+
+      wrappsProsucts.forEach((wrapp, index) => {
+        productsData[index].forEach(item => {
+          productCard(wrapp, {...item});
+        });
+      });
+
+      setTimeout(slider, 300);
+
+      
+    })
+    .catch((err) => console.log(err));
+};
+
+export default createPoducts;
